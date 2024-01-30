fb97fdc2443e687c4948bfcd44a01f05f219253f
taiebhossain <taieb.hossain006@gmail.com>
2024-01-30 13:36:27 +0600

    use hardcore value to ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,66 @@
-import Scroll from './Scroll';
-import StopWatch from './StopWatch';
+import { useRef } from 'react';
 
-export default function TaskApp() {
+export default function CatFriends() {
+  const firstCatRef = useRef(null);
+  const secondCatRef = useRef(null);
+  const thirdCatRef = useRef(null);
+
+  function handleScrollToFirstCat() {
+    firstCatRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center',
+    });
+  }
+
+  function handleScrollToSecondCat() {
+    secondCatRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center',
+    });
+  }
+
+  function handleScrollToThirdCat() {
+    thirdCatRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center',
+    });
+  }
+
   return (
-    <div className='relative top-20 left-20'>
-      <StopWatch />
-      <Scroll />
-    </div>
+    <>
+      <nav>
+        <button onClick={handleScrollToFirstCat}>Tom</button>
+        <button onClick={handleScrollToSecondCat}>Maru</button>
+        <button onClick={handleScrollToThirdCat}>Jellylorum</button>
+      </nav>
+      <div>
+        <ul>
+          <li>
+            <img
+              src='https://placekitten.com/g/200/200'
+              alt='Tom'
+              ref={firstCatRef}
+            />
+          </li>
+          <li>
+            <img
+              src='https://placekitten.com/g/300/200'
+              alt='Maru'
+              ref={secondCatRef}
+            />
+          </li>
+          <li>
+            <img
+              src='https://placekitten.com/g/250/200'
+              alt='Jellylorum'
+              ref={thirdCatRef}
+            />
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
